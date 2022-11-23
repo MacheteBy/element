fa256b3d50f47559eee56cca2eea4656aaab0f45
MacheteBy <Macheteby@gmail.com>
2022-11-23 10:47:54 +0300

    vers1

---
+++ js/script.js
@@ -0,0 +1,48 @@
+'use strict';
+
+const box = document.getElementById('box'),
+    bths = document.getElementsByTagName('button'),
+    circles = document.getElementsByClassName('circle'),
+    hearts = document.querySelectorAll('.heart'),
+    oneHeart = document.querySelector('.heart'),
+    wrapper = document.querySelector('.wrapper');
+// box.style.backgroundColor = 'blue';
+// box.style.width = '500px';
+
+box.style.cssText = 'background-color: blue; width: 500px';
+
+bths[1].style.borderRadius = '100%';
+circles[0].style.backgroundColor = 'red';
+
+// for (let i = 0; i < hearts.length; i++) {
+//     hearts[i].style.backgroundColor = 'black';
+// };
+
+hearts.forEach(item => {
+    item.style.backgroundColor = "yellow";
+});
+
+const div = document.createElement('div');
+// const text = document.createTextNode('Тут был я');
+
+div.classList.add('black');
+wrapper.append(div);
+// wrapper.appendChild(div);
+
+// wrapper.prepend(div);
+
+// // hearts[0].before(div);
+// hearts[0].after(div);
+// wrapper.insertBefore(div, hearts[1]);
+// wrapper.removeChild(hearts[1]);
+
+// circles[0].remove();
+
+// hearts[0].replaceWith(circles[0]);
+// wrapper.replaceChild(circles[1], hearts[2]);
+
+// div.innerHTML = "<h1>Hello World</h1>";
+
+// div.textContent = "<h1>Hello World</h1>";
+
+div.insertAdjacentHTML("afterend", '<h2>Hello</h2>')
